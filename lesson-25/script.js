6d3ec0d6d25133bba32f4b927eed83bedc175bb2
evgeniy_petrenko <pei9293@gmail.com>
2022-09-11 14:55:46 +0200

    students api

---
+++ lesson-25/script.js
@@ -0,0 +1,104 @@
+const STUDENT_ITEM_CLASS = 'student-item';
+const DELETE_BTN_CLASS = 'deleteBtn';
+const STUDENT_NAME_CLASS = 'student-name';
+const STUDENT_MARK_CLASS = 'student-mark';
+
+const $studentList = $('.students-list');
+const $addStudentForm = $('#create-student');
+const $studentNameInput = $('#name');
+
+$addStudentForm.on('submit', onFormSubmit);
+$studentList.on('click', `.${DELETE_BTN_CLASS}`, onDeleteBtnClick);
+$studentList.on('focusout',`.${STUDENT_MARK_CLASS}` ,onInputFocusOut);
+
+StudentsApi.getStudentsList().then(renderStudentsList);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const newStudent = getStudent();
+
+    if (newStudent.name === null | newStudent.name === undefined | newStudent.name === '') {
+    alert('Empty field')
+    return
+    }
+
+    StudentsApi.create(newStudent)
+    .then(newStudent => {
+        renderStudentItem(newStudent);
+        clearForm();
+    });
+}
+
+function onDeleteBtnClick(e) {
+    const $studentItem = getStudentItem(e.target);
+    const studentItemId = getStudentItemId($studentItem)
+    
+    StudentsApi.delete(studentItemId);
+    $studentItem.remove();
+}
+
+function onInputFocusOut(e) {
+    const $studentItem = getStudentItem(e.target);
+    const studentItemId = getStudentItemId($studentItem);
+
+    const editedMarksArr = getEditedMarksArr($studentItem);
+    StudentsApi.edit(studentItemId, {marks: editedMarksArr})
+}
+
+function getEditedMarksArr(el) {
+    const arr = [];
+    for (let i = 2; i < 12; i++) {
+        arr.push(el.find(`:nth-child(${i})`).val())
+    };
+    return arr
+}
+
+function getStudent() {
+    return newStudent = {
+        name: $studentNameInput.val(),
+        marks: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    }
+}
+
+function getStudentItem(el) {
+    return $(el.closest(`.${STUDENT_ITEM_CLASS}`));
+}
+
+function getStudentItemId($studentItem) {
+    return $studentItem.data('id')
+}
+
+function renderStudentsList(list) {
+    const html = list.map(generateStudentHtml);
+
+    $studentList.html(html);
+}
+
+function renderStudentItem(student) {
+    const newStudentHtml = generateStudentHtml(student);
+
+    $studentList.append(newStudentHtml);
+}
+
+function generateStudentHtml(student) {
+    return `
+        <li class="${STUDENT_ITEM_CLASS}" data-id ="${student.id}">
+            <span class="${STUDENT_NAME_CLASS}">${student.name}</span>
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[0]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[1]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[2]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[3]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[4]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[5]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[6]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[7]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[8]}">
+            <input type="text" class="${STUDENT_MARK_CLASS}" value="${student.marks[9]}">
+            <button class="deleteBtn">Delete</button>
+        </li>
+    `;
+}
+
+function clearForm() {
+    $addStudentForm[0].reset();
+}
